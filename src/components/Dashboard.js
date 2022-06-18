7a9a6059928c9129f5bba41e1f737644f55868fc
Seun Oshitade <seun.oshitade@gmail.com>
2022-06-18 09:27:48 -0600

    Handle state of panels

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -29,22 +29,33 @@ const data = [
 
 class Dashboard extends Component {
   state = {
-    loading: false
+    loading: false,
+    focused: null
   };
 
+  selectPanel(id) {
+    this.setState({
+     focused: id
+    });
+   }
+
   render() {
-    const dashboardClasses = classnames("dashboard");
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+     });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    const panels = data.map(panel => (
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    .map(panel => (
       <Panel
         key={panel.id}
         id={panel.id}
         label={panel.label}
         value={panel.value}
+        onSelect={this.selectPanel}
       />
     ));
 
